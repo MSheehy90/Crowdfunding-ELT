d174269605b032ea827799c4ac0d353ffd787fc7
Melinda <ponenalam@gmail.com>
2022-12-02 00:18:02 -0500

    Completed Bonus

--- crowdfunding_SQL_Analysis.sql
+++ crowdfunding_SQL_Analysis.sql
@@ -20,8 +20,9 @@ SELECT * FROM backer_count_cf_id,
 SELECT 
 	first_name,
 	last_name,
-	email
---INTO amount_left_by_contact 
+	email,
+	SUM(goal) - SUM(pledged) AS "Remaining"
+INTO amount_left_by_contact 
 FROM campaign as ca
 RIGHT JOIN contacts as co
 ON co.contact_id = ca.contact_id
@@ -29,20 +30,40 @@ WHERE outcome = 'live'
 GROUP BY first_name,
 	last_name,
 	email
-
-
-
+ORDER BY "Remaining" DESC;
 
 -- Check the table
-
+SELECT * FROM amount_left_by_contact
 
 -- 4. (5 pts)
 -- Create a table, "email_backers_remaining_goal_amount" that contains the email address of each backer in descending order, 
 -- and has the first and last name of each backer, the cf_id, company name, description, 
 -- end date of the campaign, and the remaining amount of the campaign goal as "Left of Goal". 
 
+SELECT 
+	b.email,
+	b.first_name,
+	b.last_name,
+	b.cf_id,
+	ca.company_name,
+	ca.description,
+	ca.end_date,
+	SUM(ca.goal) - SUM(ca.pledged) AS "Left of Goal"
+INTO email_backers_remaining_goal_amount
+FROM campaign as ca
+RIGHT JOIN backers as b
+ON b.cf_id = ca.cf_id
+WHERE outcome = 'live'
+GROUP BY b.email,
+	b.first_name,
+	b.last_name,
+	b.cf_id,
+	ca.company_name,
+	ca.description,
+	ca.end_date
+ORDER BY "email" DESC;
 
 
 -- Check the table
 
-
+SELECT * FROM email_backers_remaining_goal_amount
